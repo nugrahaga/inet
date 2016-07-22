0de8470039d72b07bf924a535be75e14335ea982
Benjamin Seregi <benjamin@omnetpp.org>
2016-07-22 14:50:49 +0200

    copyright

--- src/inet/linklayer/ieee80211/mac/recipient/RecipientAckProcedure.h
+++ src/inet/linklayer/ieee80211/mac/recipient/RecipientAckProcedure.h
@@ -1,20 +1,22 @@
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
+// Copyright (C) 2016 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
+// along with this program; if not, see http://www.gnu.org/licenses/.
+//
 
-#ifndef __INET_ACKPROCEDURE_H
-#define __INET_ACKPROCEDURE_H
+#ifndef __INET_RECIPIENTACKPROCEDURE_H
+#define __INET_RECIPIENTACKPROCEDURE_H
 
 #include "inet/linklayer/ieee80211/mac/contract/IRateSelection.h"
 #include "inet/linklayer/ieee80211/mac/Ieee80211Frame_m.h"
@@ -60,4 +62,4 @@ class INET_API RecipientAckProcedure
 } /* namespace ieee80211 */
 } /* namespace inet */
 
-#endif // __INET_ACKPROCEDURE_H
+#endif // __INET_RECIPIENTACKPROCEDURE_H
